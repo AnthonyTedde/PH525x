b72d80f6f91c59ea70b79839f539358f0ff392d1
elpapito <antho.tedde@gmail.com>
2015-11-24 15:50:04 +0100

    transform variable name snake_case to camelCase

--- data.R
+++ data.R
@@ -20,6 +20,21 @@ for(f in files){
                  destfile = f)
     f.name <- unlist(strsplit(x = f,
                               split = '[.]'))
+    f.name <- f.name[length(f.name) - 1]
+    if(grepl('[_]', f.name)){
+        tmp <- unlist(strsplit(x = f.name,
+                               split = '[_]'))
+        for(i in 2:length(tmp)){
+            tmp[i] <- paste(toupper(substring(text = tmp[i],
+                                              first = 1,
+                                              last = 1)),
+                            substring(text = tmp[i],
+                                      first =2),
+                            sep = '')
+        }
+        f.name <- paste(tmp, collapse = '')
+    } 
+    print(f.name)
     assign(x = f.name[1],
            read.csv(f))
 } 
